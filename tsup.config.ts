df913f911db4e713f15f54367495e4502929f949
Gal Schlezinger <gal@spitfire.co.il>
2022-12-08 21:49:30 +0200

    bundle @actions/core

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entryPoints: ["src/index.ts"],
   sourcemap: true,
+  noExternal: [/^@actions\//],
   esbuildOptions(opts, _ctx) {
     opts.legalComments = "external";
   },
