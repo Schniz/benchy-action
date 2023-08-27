6fda54b1602c6fba46bd925c72824da20f1b2e1c
Gal Schlezinger <gal@spitfire.co.il>
2023-08-27 19:34:11 +0300

    print the errors coming from the server

--- src/http-client.ts
+++ src/http-client.ts
@@ -5,6 +5,19 @@ import { FileSchema } from "./config";
 import * as GenericError from "./error";
 import { debug } from "@actions/core";
 import { inspect } from "util";
+import * as Schema from "@effect/schema/Schema";
+
+const FailureSchema = Schema.struct({
+  error: Schema.literal(true),
+  message: Schema.string,
+});
+
+const SuccessSchema = Schema.struct({
+  error: Schema.literal(false),
+});
+
+const ResponseSchema = Schema.union(FailureSchema, SuccessSchema);
+const parseResponse = Schema.parse(ResponseSchema);
 
 /**
  * Create an HTTP client that is authenticated with the ID token.
@@ -31,6 +44,32 @@ export const postMetrics = (httpClient: HttpClient, metrics: FileSchema) =>
         error,
       }),
   }).pipe(
+    Effect.tap((response) =>
+      Effect.gen(function* (_) {
+        const parsed = yield* _(
+          parseResponse(response.result),
+          Effect.mapError(
+            (error) =>
+              new GenericError.GenericError({
+                error: error,
+                message: `Response is malformed`,
+              })
+          )
+        );
+        if (parsed.error) {
+          return yield* _(
+            Effect.fail(
+              new GenericError.GenericError({
+                error: new Error(
+                  `HTTP ${response.statusCode}: ${parsed.message}`
+                ),
+                message: parsed.message,
+              })
+            )
+          );
+        }
+      })
+    ),
     Effect.tap((response) =>
       Effect.sync(() =>
         debug(`got response: ${inspect(response, { depth: 10, colors: true })}`)
