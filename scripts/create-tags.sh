8bb389f9f5ecfbf8f21a752c6b1a2dfef782fe23
Gal Schlezinger <gal@spitfire.co.il>
2023-09-07 09:19:30 +0300

    modify publish script

---
+++ scripts/create-tags.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+TAGS_TO_CREATE=$(node -e 'require("./package.json").version.split(".").slice(0, -1).forEach((_, i, arr) => console.log("v" + arr.slice(0, i+1).join(".")))')
+
+echo "Creating tags: $TAGS_TO_CREATE"
+
+for tagname in $TAGS_TO_CREATE; do
+  git tag --force $tagname || true
+  git push --force-with-lease origin $tagname
+  echo "🏷  $tagname"
+done
